c1116634d686b568559ba1fb95e432221d2c3b53
StefansCode <schulz.25@gmx.de>
2026-07-03 12:00:00 +0200

    add signal class

---
+++ code/thesisCode/main/core/signal/signal.h
@@ -0,0 +1,14 @@
+#ifndef SIGNAL_H
+#define SIGNAL_H
+#include "config.h"
+#include "ringBuffer/ringBuffer.h"
+
+class Signal {
+private:
+    RingBuffer buffer;
+
+public:
+    static from()
+};
+
+#endif// SIGNAL_H
