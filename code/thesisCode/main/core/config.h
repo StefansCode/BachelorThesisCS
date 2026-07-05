223e0f267f9434aa1364b94bdbe4c2172f8b8979
StefansCode <schulz.25@gmx.de>
2026-07-05 21:26:55 +0200

    use int16

--- code/thesisCode/main/core/config.h
+++ code/thesisCode/main/core/config.h
@@ -4,9 +4,11 @@
 
 #define SAMPLE_RATE 44100
 #define BUFFER_SIZE 256
-typedef uint16_t sample;
+typedef int16_t sample;
 
 const uint8_t BITS_PER_SAMPLE = sizeof(sample) * 8;
-const sample MAX_SAMPLE_VALUE = (1 << BITS_PER_SAMPLE) - 1;
+#define SAMPLE_MAX INT16_MAX
+#define SAMPLE_MIN INT16_MIN
+#define SAMPLE_RANGE (SAMPLE_MAX - SAMPLE_MIN)
 
 #endif// CONFIG_H
