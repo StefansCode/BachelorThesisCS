37989da4f24eb90f90aacd5f3bbe5a90f2aecec9
StefansCode <schulz.25@gmx.de>
2026-07-20 10:51:26 +0200

    add util class

---
+++ code/thesisCode/main/adapter/util/util.cpp
@@ -0,0 +1,11 @@
+#include "util.h"
+
+Util util;
+
+unsigned long Util::millis() {
+  return esp_timer_get_time() / 1000;
+}
+
+void Util::delay(unsigned long ms) {…
+  vTaskDelay(ms/portTICK_PERIOD_MS);
+}
---
+++ code/thesisCode/main/adapter/util/util.h
@@ -0,0 +1,15 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <esp_timer.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+class Util {
+public:
+    unsigned long millis();
+    void delay(unsigned long ms);
+};
+
+extern Util util;
+#endif // UTIL_H
